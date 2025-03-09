a1cad07ec4577fa1855a8b8a79af5afeee1c5425
Danyelle <dkobes15@gmail.com>
2025-03-09 15:01:47 -0500

    base structure for POST in authAPI

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -2,8 +2,24 @@ import { UserLogin } from "../interfaces/UserLogin";
 
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
+  try {
+    const response = await fetch("", {
+      method: 'POST',
+      headers: {
+        'Cntent-Type': '',
+      },
+      body: JSON.stringify(userInfo),
+    });
+
+    if (!response.ok) {
+      throw new Error('Login failed');
+    }
+
+    const data = await response.json();
+    console.log('Login successful', data); 
+  } catch (error) {
+    console.log('Error during login', error);
 }
-
-
+};
 
 export { login };
