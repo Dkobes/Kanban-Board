57f3fed176e10433ff963edb37d634982be13a03
Danyelle <dkobes15@gmail.com>
2025-03-09 15:34:47 -0500

    layout for auth-routes

--- server/src/routes/auth-routes.ts
+++ server/src/routes/auth-routes.ts
@@ -5,6 +5,30 @@ import bcrypt from 'bcrypt';
 
 export const login = async (req: Request, res: Response) => {
   // TODO: If the user exists and the password is correct, return a JWT token
+  const { username, password } = req.body;
+
+  try {
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    const token = jwt.sign(
+      { username: user.username, id: user._id },
+    '', {
+      expiresIn: '1h'
+    });
+
+    return res.status(200).json({ token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Something went wrong' });
+  }
 };
 
 const router = Router();
