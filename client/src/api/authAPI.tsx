b01dd71d189a66f0f0c0d1ab10e1546ce87c3b68
Danyelle <dkobes15@gmail.com>
2025-03-09 15:17:41 -0500

    fixed spelling typo, added in fetch path

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -3,10 +3,10 @@ import { UserLogin } from "../interfaces/UserLogin";
 const login = async (userInfo: UserLogin) => {
   // TODO: make a POST request to the login route
   try {
-    const response = await fetch("", {
+    const response = await fetch("/auth/login", {
       method: 'POST',
       headers: {
-        'Cntent-Type': '',
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(userInfo),
     });
