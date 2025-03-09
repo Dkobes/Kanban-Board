05df1f1cfa28f87da9801ebd595f92607be6130e
Danyelle <dkobes15@gmail.com>
2025-03-09 15:56:56 -0500

    changed if not response code

--- client/src/api/authAPI.tsx
+++ client/src/api/authAPI.tsx
@@ -12,14 +12,17 @@ const login = async (userInfo: UserLogin) => {
     });
 
     if (!response.ok) {
-      throw new Error('Login failed');
+      const errorData = await response.json();
+      throw new Error(`Error: ${errorData.message}`);
     }
 
     const data = await response.json();
-    console.log('Login successful', data); 
-  } catch (error) {
-    console.log('Error during login', error);
+
+    return data;
+  } catch (err) {
+    console.log('Error from user login: ', err);
+    return Promise.reject('Could not fetch user info');
+  }
 }
-};
 
 export { login };
